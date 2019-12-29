f87959cec624e00c76d0696fcac6d720df6f5a79
matttrach <matttrach@gmail.com>
2019-12-29 01:10:10 -0600

    adding hellowprl

---
+++ hello/go.mod
@@ -0,0 +1,9 @@
+module matttrach/hello
+
+go 1.13
+
+require (
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20191030003036-b2a7f28a184a // indirect
+	rsc.io/quote v1.5.2
+)
---
+++ hello/hello_test.go
@@ -0,0 +1,10 @@
+package hello
+import "testing"
+
+func TestHello(t *testing.T) {
+  want := "Hello, world."
+  got  := Hello()
+  if got != want {
+    t.Errorf("Hello() = %q, want %q", got, want)
+  }
+}
